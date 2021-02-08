92956ce9460cee4f368cd975fb352f6713218b18
LucRiviere <75257840+LucRiviere@users.noreply.github.com>
2021-02-08 22:03:56 +0100

    Update MainJeuNombreADeviner.cpp

--- src/MainJeuNombreADeviner.cpp
+++ src/MainJeuNombreADeviner.cpp
@@ -1,23 +1,38 @@
 /*************************************************/
-// Nom du projet: Jeu du nombre ‡ deviner
+// Nom du projet: Jeu du nombre √† deviner
 // Nom du fichier: MainJeuNombreAdeviner.cpp
 // Version : 1.0
-// Nom du programmeur: MP Pinaud
-// Date de crÈation : 25/01/2021
-// RÙle du fichier: Contient le code jeu
+// Nom du programmeur: MP Pinaud/ Luc Rivi√®re
+// Date de cr√©ation : 08/02/2021
+// R√¥le du fichier: Contient le code jeu
 // Nom des composants utilises: JeuNombreADeviner.h
 //                              iostream
 // Historique du fichier:
+//Coder le MaiJeuNombreADevinercpp et le JeuNombreADeviner.cpp + parametrer les entrees/sorties dans le JeuNombreADeviner.h
 /*************************************************/
 #include <iostream>
-using namespace std;
+#include <ctime>
+#include <string>
+#include "JeuNombreADeviner.h"
 
-#include "../include/JeuNombreADeviner.h"
 
 int main()
 {
-    cout << "Vous allez jouer pour deviner un nombre secret" << endl;
 
-    return 0;
+    cout << "Vous allez jouer pour deviner un nombre secret" <<endl;
+    TJoueur joueur1;
+    string username;
+    cout << "Entrez votre prenom" << "\n";
+    cin>> username;
+    InitJoueur(joueur1, username);
+    cout << Nom(joueur1) <<endl;
+    int n{10};
+    int tirage = std::rand () % n;
+    JouerPartie(joueur1,tirage);
+    int nbEssais{};
+    bool gagne{};
+    MajResultatsJoueur(joueur1,nbEssais,gagne);
+
+
+  return 0;
 }
-
